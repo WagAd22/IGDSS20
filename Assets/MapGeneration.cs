f9d81322923ad0ce9e3aa8a0ba08e7a9d346f3e3
hka007 <hamoudAzzam007@gmail.com>
2020-05-10 14:29:19 +0200

    Assingment 2

---
+++ Assets/MapGeneration.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class MapGeneration : MonoBehaviour
+{
+    public Texture2D hightmap;
+
+    public Transform waterPrefab;
+    public Transform sandPrefab;
+    public Transform grassPrefab;
+    public Transform forcePrefab;
+    public Transform mountainPrefab;
+    public Transform stonePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var width = hightmap.width;
+        var height = hightmap.height;
+
+        float[,] x_pixels = new float[width, height];
+
+        float x = 0;
+        float z = -5;
+
+        // read the pixel in hightmap and to note that the surface will be create in ZX surface
+        // the poisition of tiles will create as following 
+        // it start with (0 -5) and then create all the tiles with x = 0 and increamnt the index of Z with 10
+        // when the Z is eqaul the heigh the value of x will increamnt with 8.5
+        // it check also if the index is odd or even that will help to arrange the tiles as they are in the Assignment
+        // (0 -5)  (0 5)    (0 15)   (0 25)
+        // (8.5 0) (8,5 10) (8.5 20) (8.5 30)
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                x_pixels[j, i] = hightmap.GetPixel(j, i).r;
+                Generat_Tile(x_pixels[j, i], -x, z);// Generate the Map according to the value of the pixel in the Hightmap
+                z += 10;
+            }
+            x -= 8.5f;
+
+            if (i % 2 != 0)
+                z = -5;
+            else
+                z = 0;
+        }
+    }
+
+    private void Generat_Tile(float pixel, float i, float j)
+    {
+        if (pixel == 0.0f)
+        {
+            //0.0 - Water
+            Instantiate(waterPrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+        else if (pixel > 0.0f && pixel < 0.2f)
+        {
+            //0.0 to 0.2 - Sand
+            Instantiate(sandPrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+        else if (pixel >= 0.2f && pixel < 0.4f)
+        {
+            //0.2 to 0.4 - Grass
+            Instantiate(grassPrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+        else if (pixel >= 0.4f && pixel < 0.6f)
+        {
+            //0.4 to 0.6 - Forest
+            Instantiate(forcePrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+        else if (pixel >= 0.6f && pixel < 0.8f)
+        {
+            //0.6 to 0.8 - Stone
+            Instantiate(stonePrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+        else if (pixel >= 0.8f && pixel < 1.0f)
+        {
+            //0.8 to 1.0 - Mountain
+            Instantiate(mountainPrefab, new Vector3(i, 50f * pixel, j), Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
 using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 mousePos;
+    float limitX;
+    float limitZ;
+    float x;
+    float z; // z = y
+    float zoom = 1f;
+
+    Camera myCamera;
     void Start()
     {
-        
-    }
+        limitZ = transform.position.z;
+        limitX = transform.position.y;
 
-    // Update is called once per frame
+        myCamera = GetComponent<Camera>();
+    }
     void Update()
     {
-        
+        // to be clear with that the x is y and z is x that's becauase the coordinate is ZX not XZ
+        // the Y is constant because it's the height of the Camera which we dont need it to move the camera
+        // in the ZX surface
+        if (Input.GetMouseButton(1))
+        {
+
+            x = Mathf.Clamp(transform.position.x + 5f * Input.GetAxis("Mouse Y"), limitX - limitX / 2, limitX + limitX / 2);
+            z = Mathf.Clamp(transform.position.z - 5f * Input.GetAxis("Mouse X"), limitZ - limitZ / 2, limitZ + limitZ / 2);
+
+            this.transform.position = new Vector3(x, 200, z);
+        }
+
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            myCamera.fieldOfView += zoom;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            myCamera.fieldOfView -= zoom;
+        }
+
+        myCamera.fieldOfView = Mathf.Clamp(myCamera.fieldOfView, 20, 50);
+
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                var select = hit.transform;
+                var selectrender = select.GetComponent<Renderer>();
+
+                Debug.DrawRay(select.position, Vector3.up * 250,Color.red);
+                Debug.Log(selectrender.name);
+            }
+        }
     }
+
+
 }
